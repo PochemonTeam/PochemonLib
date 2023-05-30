a82b40f76e7825bfa96bdccc83129c3ac079a8a2
FranCHEVAL <francois.cheval@cpe.fr>
2023-05-30 15:04:34 +0200

    first commit - Add DTO

---
+++ pochemonlib/pochemonlib/src/test/java/pochemon/pochemonlib/PochemonlibApplicationTests.java
@@ -0,0 +1,13 @@
+package pochemon.pochemonlib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PochemonlibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
