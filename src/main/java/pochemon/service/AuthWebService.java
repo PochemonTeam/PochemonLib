f9ca4a397aa776ed040442bfeeea453d49fde6c0
GAUCHOUX Xavier <xavier.gauchoux@soprasteria.com>
2023-06-01 16:26:09 +0200

    Updated AuthWebService

--- src/main/java/pochemon/service/AuthWebService.java
+++ src/main/java/pochemon/service/AuthWebService.java
@@ -1,9 +1,16 @@
 package pochemon.service;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import lombok.Setter;
+import pochemon.dto.AuthDTO;
+
+@Setter
 @Component
 public class AuthWebService {
 
@@ -18,13 +25,27 @@ public class AuthWebService {
 
     public String login(String username, String password) {
         String url = apiUrl + "/auth/login";
+        
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        
+        AuthDTO authDTO = new AuthDTO(username, password);
+        
+        HttpEntity<AuthDTO> requestEntity = new HttpEntity<>(authDTO, headers);
 
-        return restTemplate.postForObject(url, null, String.class);
+        return restTemplate.postForObject(url, requestEntity, String.class);
     }
 
-    public String register(String username, String password) {
+    public void register(String username, String password) {
         String url = apiUrl + "/auth/register";
+        
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        
+        AuthDTO authDTO = new AuthDTO(username, password);
+        
+        HttpEntity<AuthDTO> requestEntity = new HttpEntity<>(authDTO, headers);
 
-        return restTemplate.postForObject(url, null, String.class);
+        restTemplate.postForObject(url, requestEntity, String.class);
     }
 }
